784d93759264de4a446d6ca930381945a316e615
dee080999 <deeksha.parul68@gmail.com>
2021-09-10 12:16:36 +0530

    added show pages2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-tag-spacing */
+/* eslint-disable react/jsx-indent */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import {Switch, Route} from 'react-router-dom';
 
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
-
+import Show from "./pages/Show";
 
 function App() {
   return ( 
@@ -15,6 +18,10 @@ function App() {
         </Route>
         <Route exact path="/starred">
         <Starred/>
+        </Route>
+
+        <Route exact path="/show/:id">
+          <Show/>
         </Route>
   
         <Route><div>Not Found</div></Route>
---
+++ src/pages/Show.js
@@ -0,0 +1,53 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-indent */
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router'
+import { apiGet } from '../misc/config';
+
+
+const Show = () => {
+
+    const { id } = useParams();
+
+    const [show, setShow]= useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const[error, setError]= useState(null);
+
+    useEffect(() => {
+
+      let isMounted = true;
+
+      apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+
+      
+           if(isMounted){
+            setShow(results);
+            setIsLoading(false);
+           }
+        
+      
+      
+      }).catch(err => {
+          if(isMounted){
+            setError(err.message);
+            setIsLoading(false);
+          }
+          
+      });
+
+      return() =>{
+          isMounted = false;
+      }
+}, [id]);
+
+if(isLoading){
+    return <div>Data is being loaded</div>
+}
+
+if(error){
+    return<div>Error occurred:{error}</div>;
+}
+  return <div>this is show page</div>  
+};
+
+export default Show
