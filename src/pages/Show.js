8f06552cf36c21cc4dd77e14ab07ec03daa09d94
dee080999 <deeksha.parul68@gmail.com>
2021-09-13 12:00:24 +0530

    styled the pages

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-indent */
 import React, {useEffect, useReducer} from 'react'
 import { useParams } from 'react-router'
 import { apiGet } from '../misc/config';
-
-
+import ShowMainData from '../components/show/ShowMainData';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import { ShowPageWrapper, InfoBlock } from './Show.Styled';
 
 const reducer= (prevState,action)=> {
  switch (action.type) {
@@ -35,6 +40,7 @@ const Show = () => {
 
    const [{show, isLoading,error},dispatch] = useReducer(reducer, initialState )
 
+  
    
 
     useEffect(() => {
@@ -69,7 +75,24 @@ if(isLoading){
 if(error){
     return<div>Error occurred:{error}</div>;
 }
-  return <div>this is show page</div>  
-};
+
+  return ( 
+  <ShowPageWrapper>
+      <ShowMainData image={show.image} name={show.name} rating={show.rating} summary={show.summary} tags={show.genres} />
+  <InfoBlock>
+          <h2>Details</h2>
+          <Details status={show.status} network={show.network} premiered={show.premiered} />
+     </InfoBlock>
+     <InfoBlock>
+              <h2>Seasons</h2>
+              <Seasons seasons={show._embedded.seasons} />
+          </InfoBlock>
+          <InfoBlock>
+              <h2>Cast</h2>
+              <Cast cast={show._embedded.cast} />
+          </InfoBlock> 
+  </ShowPageWrapper> 
+  )
+  };
 
 export default Show
